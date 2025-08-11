ce9a6f03fd8a5bd2143c9ea2b397599fe92a6223
Lokeshk66 <kadireppagarilokesh66@gmail.com>
2025-08-11 12:42:51 +0530

    Describe what you updated

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './App';
-import './index.css';
+
 
 const theme = createTheme({
   palette: {
